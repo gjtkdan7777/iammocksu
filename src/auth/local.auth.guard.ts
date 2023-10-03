a4a58dbab9a88c2ae8b223b086a992e86414b56b
gjtkdan7777 <tkdan7777@naver.com>
2023-10-03 20:00:18 +0900

    radis login test

--- src/auth/local.auth.guard.ts
+++ src/auth/local.auth.guard.ts
@@ -7,9 +7,6 @@ export class LocalAuthGuard extends AuthGuard('local') {
       const result = (await super.canActivate(context)) as boolean;
       const request = context.switchToHttp().getRequest();
       await super.logIn(request);
-      console.log(1234)
-      console.log(1234)
-      console.log(1234)
       return result;
     }
 }
